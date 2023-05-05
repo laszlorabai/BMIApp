d6f87efe57e896a14a02b91afa7df905f30a3409
Rábai László <h050968@stud.u-szeged.hu>
2023-05-05 04:32:48 +0200

    First test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText userNameET;
+    EditText passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText userNameET = findViewById(R.id.editTextUserName);
+        EditText passwordET = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view){
+
+
+        String userName = userNameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", jelszo:" + password );
+    }
+
+
+    public void register(View view) {
+    }
+}
